4e8333fee7d61f67bab6752b3ef250e4902ac6ff
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 10:26:53 +0900

    Refactor Auth component to implement a structured login form with email and password fields

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,14 +1,31 @@
 import { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, Typography, TextField, Button } from "@mui/material";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
 
   return (
     <>
-      <h2>Auth</h2>
+      <Typography variant="h2" component="h2">
+        Login Form
+      </Typography>
 
-      <Box component="form"></Box>
+      <Box component="form" sx={{ mt: 2 }}>
+        <TextField fullWidth label="Email Address" type="text" name="email" variant="outlined" />
+
+        <TextField
+          sx={{ mt: 2 }}
+          fullWidth
+          label="Password"
+          type="email"
+          name="email"
+          variant="outlined"
+        />
+
+        <Button sx={{ mt: 2 }} type="submit" variant="contained">
+          Login
+        </Button>
+      </Box>
     </>
   );
 }
