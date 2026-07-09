4cf461b5bf2edbeb665ab5bccf782a489767decd
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 11:32:41 +0900

    Implement user registration functionality in Auth component

--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { Box, Typography, TextField, Button } from "@mui/material";
+import { authService } from "../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
   const [newAccount, setNewAccount] = useState(true);
@@ -7,9 +9,7 @@ function Auth() {
     email: "",
     password: "",
   });
-
-  console.log(form);
-
+  const auth = authService;
   const handleChange = e => {
     const { name, value } = e.target;
 
@@ -18,14 +18,35 @@ function Auth() {
       [name]: value,
     }));
   };
+  const onSubmit = e => {
+    e.preventDefault();
+
+    if (newAccount) {
+      // 회원가입
+      createUserWithEmailAndPassword(auth, form.email, form.password)
+        .then(userCredential => {
+          // Signed up
+          const user = userCredential.user;
+          // ...
+        })
+        .catch(error => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+
+          console.log(errorCode, errorMessage);
+        });
+    } else {
+      // 로그인
+    }
+  };
 
   return (
     <>
       <Typography variant="h2" component="h2">
-        Login Form
+        {newAccount ? "회원가입 폼" : "로그인 폼"}
       </Typography>
 
-      <Box component="form" sx={{ mt: 2 }}>
+      <Box component="form" sx={{ mt: 2 }} onSubmit={onSubmit}>
         <TextField
           fullWidth
           label="Email Address"
@@ -46,7 +67,7 @@ function Auth() {
         />
 
         <Button sx={{ mt: 2 }} type="submit" variant="contained">
-          Login
+          {newAccount ? "회원가입" : "로그인"}
         </Button>
       </Box>
     </>
