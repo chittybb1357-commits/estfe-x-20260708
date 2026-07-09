0a6313c57e6d164746b4d323da10242d12101fba
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 09:25:53 +0900

    Comment out unused imports and console logs in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App.jsx";
-import { firebase } from "./firebase.js";
+// import { firebase } from "./firebase.js";
 import { BrowserRouter } from "react-router";
 
-console.log(firebase);
+// console.log(firebase);
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
