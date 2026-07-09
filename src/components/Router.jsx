a103eb900f06014ec4aac830326e5f1d978e1289
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-09 13:51:22 +0900

    Add profile route and conditional navigation in Router component

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,23 @@
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
+import Nav from "./Nav";
+import Profile from "../routes/Profile";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 function Router({ isLoggedIn }) {
   return (
     <>
+      {isLoggedIn && <Nav />}
+
       <Routes>
-        {isLoggedIn ? <Route path="/" element={<Home />} /> : <Route path="/" element={<Auth />} />}
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<profile />} />
+          </>
+        ) : (
+          <Route path="/" element={<Auth />} />
+        )}
       </Routes>
     </>
   );
