c894633eb7356aed6a9fa5574497ccabaecd6a21
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-13 11:40:13 +0900

    Refactor Home component to use Comment component for rendering comments

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -21,6 +21,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebase";
 import { useEffect, useState } from "react";
+import Comment from "../components/Comment";
 
 function Home() {
   const [comment, setComment] = useState("");
@@ -87,14 +88,9 @@ function Home() {
 
       <Divider sx={{ my: 3 }} />
 
-      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+      <List sx={{ width: "100%" }}>
         {comments.map(item => (
-          <ListItem key={item.id} alignItems="flex-start" divider>
-            <ListItemText
-              primary={item.comment}
-              secondary={item.date?.toDate ? item.date.toDate().toLocaleString() : "작성시간 없음"}
-            />
-          </ListItem>
+          <Comment key={item.id} item={item} />
         ))}
       </List>
     </>
