a23ffa78e274608aa3d0478bd5a855ed8adcb84c
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-13 13:39:31 +0900

    Add initialization state and user ID handling in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,19 +7,21 @@ import { onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [init, setInit] = useState(false); //초기화 전
+  const [userId, setUserId] = useState(null);
   const auth = authService;
 
   useEffect(() => {
     onAuthStateChanged(auth, user => {
       if (user) {
-        const uid = user.uid;
+        // const uid = user.uid;
 
-        console.log(uid);
-
+        setUserId(user.uid);
         setIsLoggedIn(true);
       } else {
         setIsLoggedIn(false);
       }
+      setInit(true);
     });
   }, [auth]);
 
@@ -28,7 +30,7 @@ function App() {
       <Container>
         <h1>ESTFE-X</h1>
 
-        <Router isLoggedIn={isLoggedIn} />
+        {init ? <Router isLoggedIn={isLoggedIn} userId={userId} /> : <h2>초기화 중입니다.</h2>}
       </Container>
     </>
   );
