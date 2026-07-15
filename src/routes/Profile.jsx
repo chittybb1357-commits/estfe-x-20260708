2fa710755b35193ff99616d39b760126b1f1e3a8
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-15 13:49:08 +0900

    Add comments retrieval and display in Profile component

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -1,11 +1,34 @@
 import { signOut } from "firebase/auth";
-import { authService } from "../firebase";
-import { Typography, Button } from "@mui/material";
+import { authService, db } from "../firebase";
+import { Typography, Button, Divider, List } from "@mui/material";
 import { useNavigate } from "react-router";
+import { useEffect, useState, useRef } from "react";
+import { collection, query, where, getDocs, orderBy, onSnapshot } from "firebase/firestore";
+import Comment from "../components/Comment";
 
 function Profile() {
   const auth = authService;
+  const userId = auth.currentUser.uid;
   const navigate = useNavigate();
+  const [comments, setComments] = useState([]);
+  const getComments = async () => {
+    const q = query(
+      collection(db, "comments"),
+      where("uid", "==", userId),
+      orderBy("date", "desc"),
+    );
+
+    onSnapshot(q, querySnapshot => {
+      const commentsArray = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+      setComments(commentsArray);
+    });
+  };
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
   const onLogout = () => {
     signOut(auth)
       .then(() => {
@@ -24,6 +47,14 @@ function Profile() {
       <Button sx={{ mt: 2 }} type="button" variant="contained" onClick={onLogout}>
         로그아웃
       </Button>
+
+      <Divider sx={{ my: 3 }} />
+
+      <List sx={{ width: "100%" }}>
+        {comments.map(item => (
+          <Comment key={item.id} item={item} isShown={userId === item.uid} />
+        ))}
+      </List>
     </>
   );
 }
